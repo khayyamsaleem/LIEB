d52e5ecb1ec695fd2bd010839b259f560fb24b66
Christopher Hittner <chrishittner@gmail.com>
2018-04-15 13:02:33 -0400

    Modified mongo.js export to require db/collection
    I honestly based the initial template on the homework code.
    I have modifed mongo.js to reflect a more generalized scheme.

--- mongo.js
+++ mongo.js
@@ -7,16 +7,16 @@ let _connection = undefined;
 let _db = undefined;
 
 // Gets the database connection
-async function get() {
+async function get(db_name, coll_name) {
     if (_connection === undefined) {
         console.log("Connecting to db...");
-        _connection = await client.connect(settings.dbconf.url);
+        _connection = await client.connect("mongodb://localhost:27017");
         console.log("Accessing database...");
-        _db = await _connection.db(settings.dbconf.db);
+        _db = await _connection.db(db_name);
     }
 
     console.log("Accessing collection...");
-    return await _db.collection(settings.dbconf.collection);
+    return await _db.collection(coll_name);
 }
 
 module.exports = get;
