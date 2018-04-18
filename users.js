0a0a9307b58c60bc23684787a0f09977043345d5
Christopher Hittner <chrishittner@gmail.com>
2018-04-18 17:47:15 -0400

    Authentication and enhancements

--- users.js
+++ users.js
@@ -1,4 +1,6 @@
 const mongo = require('./mongo');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 async function getUser (username) {
     // Get the user collection
@@ -6,7 +8,12 @@ async function getUser (username) {
     
     // Get the user
     try {
-        return await users.findOne({"username" : username});
+        let user = await users.findOne({"username" : username});
+
+        // We will hide the password data
+        user.passwd = undefined;
+
+        return user;
     } catch (ex) {
         return undefined;
     }
@@ -15,6 +22,9 @@ async function getUser (username) {
 async function createUser (user) {
     // Get the user collection
     const users = await mongo("database", "users");
+
+    // No sessions by default
+    user.sessions = []
 
     // Add the user
     users.insert(user);
@@ -25,51 +35,68 @@ async function updateProfile (user, profileChanges) {
     const users = await mongo("database", "users");
     
     users.updateOne(user, profileChanges);
-    
 }
 
 async function loginUser (username, password) {
+    // Get the user collection
+    const users = await mongo("database", "users");
+    
+    // Get the user
+    try {
+        let user = await users.findOne({"username" : username});
 
+        let valid = await bcrypt.compare(password, phash);
+
+        if (valid) {
+            // Create an id for the session.
+            sess_id = uuid();
+            
+            // Add to the session list
+            const sessions = await mongo("database", "sessions");
+            users.update({"username" : username}, {"$push" : { "sessions" : sess_id }});
+            sessions.insert({"_id" : sess_id, "username" : username});
+            
+            return sess_id;
+        } else
+            return undefined;
+
+    } catch (ex) {
+        return undefined;
+    }
 }
 
 async function logoutUser (username, sessionId) {
+    // Get the user collection
+    const users = await mongo("database", "users");
+    
+    try {
+        // Remove the session 
+        const sessions = await mongo("database", "sessions");
+        users.update({"username" : username}, {"$pull" : { "sessions" : sessionId }});
+        sessions.remove({"_id" : sess_id});
 
+    } catch (ex) {}
 }
 
 async function addSubscription (username, userToSub) {
     // Get the user collection
     const users = await mongo("database", "users");
+    if (users === undefined)
+        return;
     
-    // Get the user
-    let user = await getUser(username);
-
-    if (user.subscription_list.indexOf(userToSub) < 0) {
-        // Add the subscriber
-        user.subscription_list.push(userToSub);
-        
-        // Generate and apply the update
-        let update = {"subscription_list" : user.subscription_list};
-        updateProfile({"username" : username}, update);
-    }
-
+    try {
+        users.update({"username" : username}, {"$push" : {"subscription_list" : userToSub["_id"]}});
+    } catch (ex) {}
 }
 
 async function removeSubscription (username, userToUnsub) {
     // Get the user collection
     const users = await mongo("database", "users");
-    
-    // Get the user
-    let user = await getUser(username);
-    
-    let idx = user.subscription_list.indexOf(userToSub);
-    if (idx >= 0) {
-        // Remove the item
-        user.subscription_list.splice(idx, 1);
 
-        // Generate and apply the update
-        let update = {"subscription_list" : user.subscription_list};
-        updateProfile({"username" : username}, update);
-    }
+    // Remove the item
+    try {
+        users.update({"username" : username}, {"$pull" : {"subscription_list" : userToUnsub["_id"]}});
+    } catch (ex) {}
 }
 
 module.exports = {
